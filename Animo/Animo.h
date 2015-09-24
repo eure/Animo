eb7bc3454c42a520334a3412cdab508262de530f
John Rommel Estropia <rommel.estropia@gmail.com>
2015-09-25 01:23:26 +0900

    Initial Commit

---
+++ Animo/Animo.h
@@ -0,0 +1,19 @@
+//
+//  Animo.h
+//  Animo
+//
+//  Created by John Rommel Estropia on 2015/09/25.
+//  Copyright © 2015 John Rommel Estropia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animo.
+FOUNDATION_EXPORT double AnimoVersionNumber;
+
+//! Project version string for Animo.
+FOUNDATION_EXPORT const unsigned char AnimoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animo/PublicHeader.h>
+
+
---
+++ AnimoTests/AnimoTests.swift
@@ -0,0 +1,36 @@
+//
+//  AnimoTests.swift
+//  AnimoTests
+//
+//  Created by John Rommel Estropia on 2015/09/25.
+//  Copyright © 2015 John Rommel Estropia. All rights reserved.
+//
+
+import XCTest
+@testable import Animo
+
+class AnimoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
